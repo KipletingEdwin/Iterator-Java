19f3730bddcb85b061401fe366249371eaf3cddd
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-06-05 14:57:43 +0300

    finalize on the code

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,43 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        HashSet<String> foods = new HashSet<String>();
 
+        foods.add("Pizza");
+        foods.add("Ice Cream");
+        foods.add("bourbon chicken");
+
+
+        Iterator<String> it = foods.iterator();
+
+        while (it.hasNext()){
+            System.out.println(it.next());
         }
+
+
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        nums.add(23);
+        nums.add(213);
+        nums.add(123);
+        nums.add(233);
+
+        Iterator<Integer> ite = nums.iterator();
+
+        while (ite.hasNext()){
+            int i = ite.next();
+
+            if(i < 150){
+                ite.remove();
+            }
+        }
+        System.out.println(nums);
+
+
     }
 }
